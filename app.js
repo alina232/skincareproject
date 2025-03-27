99d96f936c8572bcef71d7ef92f314b67df36b07
alina232 <timilsina.alina12@gmail.com>
2025-03-27 12:53:00 -0400

    Mongodb connection made

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/SkinCareProject',{useNewUrlParser: true},{useUnifiedTopology: true});
+const db = mongoose.connection;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
