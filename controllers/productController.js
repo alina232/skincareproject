a7475fa76d921491dd5f66dec81ef355cac8dcc2
alina232 <timilsina.alina12@gmail.com>
2025-03-30 17:35:34 -0400

    get productById added

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,3 +11,22 @@ exports.getProducts = async (req, res) => {
         });
     }
 };
+
+exports.getProductById = async (req,res) => {
+    const { id } = req.params;
+    try{
+        const product = await Product.findOne({ProductId: id});
+        if(!product){
+            return res.status(404).json({
+                message: "Product id not found",
+                error: error.message
+            });
+        }
+        res.status(200).json(product);
+    } catch(error) {
+        return res.status(500).json({
+            message: "Error fetching Product id",
+            error: error.message
+        });
+    }
+};
